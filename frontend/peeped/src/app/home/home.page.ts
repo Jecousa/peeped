2dd39bc069b913e64b6e65665aac5574dc10d165
Jake <jecolousa@gmail.com>
2020-03-19 18:39:45 -0600

    working map

--- frontend/peeped/src/app/home/home.page.ts
+++ frontend/peeped/src/app/home/home.page.ts
@@ -1,26 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ModalController, NavController, IonRouterOutlet } from '@ionic/angular';
 import { WelcomeModalPage } from './welcome-modal/welcome-modal.page';
+import { map } from 'rxjs/operators';
+import { Plugins } from '@capacitor/core';
+const { Geolocation } = Plugins;
+
+import { Observable } from 'rxjs';
+
+declare var google;
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit{
+export class HomePage implements OnInit {
+  @ViewChild('map', {static: false}) mapElement: ElementRef;
+   map: any;
 
-  constructor(public navCtrl: NavController, public modalController: ModalController, private routerOutlet: IonRouterOutlet) {}
-ngOnInit(){
-  this.presentModal()
+constructor(public navCtrl: NavController, public modalController: ModalController, private routerOutlet: IonRouterOutlet) {}
+ngOnInit() {
+  this.presentModal();
 }
-  async presentModal(){
+ionViewWillEnter() {
+  this.loadMap();
+}
+loadMap() {
+  let latLng = new google.maps.LatLng(51.9036442, 7.6673267);
+
+  let mapOptions = {
+    center: latLng,
+    zoom: 5,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+}
+  async presentModal() {
   const modal = await this.modalController.create({
     component: WelcomeModalPage,
-    componentProps: {
-      'firstName': 'Douglas',
-      'lastName': 'Adams',
-      'middleInitial': 'N'
-    },
     swipeToClose: true,
     presentingElement: await this.modalController.getTop()
   });
