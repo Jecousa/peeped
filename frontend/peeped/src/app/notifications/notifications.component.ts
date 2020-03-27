963657b84c47bfbd910f7302ab4466b780f7c0a4
Jakob Essig <admin@5280-dev.io>
2020-03-27 11:49:55 -0600

    messages

--- frontend/peeped/src/app/notifications/notifications.component.ts
+++ frontend/peeped/src/app/notifications/notifications.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Input, Renderer2, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
   styleUrls: ['./notifications.component.scss'],
 })
-export class NotificationsComponent implements OnInit {
+export class NotificationsComponent implements AfterViewInit {
+  @ViewChild('messageWrapper', { read: ElementRef}) messageWrapper: ElementRef;
+  @Input("expanded") expanded: boolean = false;
+  @Input("expandedHeight") expandHeight: string = "150px";
 
-  constructor() { }
+  constructor(public renderer: Renderer2) { }
 
-  ngOnInit() {}
-
+ngAfterViewInit() {
+  this.renderer.setStyle(this.messageWrapper.nativeElement, "max-height", this.expandHeight);
+}
 }
